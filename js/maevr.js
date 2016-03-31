651abdcc2c1fcca0d9863f76e00a4f67d1cfa95f
Scott Garner <scott@j38.net>
2016-03-30 23:40:19 -0500

    Small fix for #12

--- js/maevr.js
+++ js/maevr.js
@@ -177,7 +177,7 @@ var MAEVR = {
 
         var audioStartTime = 0;
 
-        if (!isNaN(parseInt(window.location.hash.substr(1))) {
+        if (!isNaN(parseInt(window.location.hash.substr(1)))) {
           audioStartTime = window.location.hash.substr(1);
         }
 

f1473d3e31fe26ed20fe44ab9f5506ac0b4f8e56
Scott Garner <scott@j38.net>
2016-04-04 21:20:02 -0500

    Removing stem stuff from main js file.

--- js/maevr.js
+++ js/maevr.js
@@ -330,32 +330,6 @@ MAEVR.Message = {
 
 //
 
-MAEVR.Stems = {
-  chunkCount: 30,
-  getStemValue: function(stemIndex) {
-
-    if (stemIndex >= stems.length) return null;
-    if (MAEVR.elapsedTime == 0) return 0;
-
-    var stem = stems[stemIndex];
-    var sampleIndex = (MAEVR.elapsedTime / 1000) * this.chunkCount;
-
-    var previousIndex = Math.floor(sampleIndex);
-    var nextIndex = Math.ceil(sampleIndex);
-
-    var progress = sampleIndex % Math.floor(sampleIndex);
-
-    var previousValue = stem[previousIndex];
-    var nextValue = stem[nextIndex];
-
-    var lerpValue =  previousValue + progress * (nextValue - previousValue);
-
-    return lerpValue;
-  }
-}
-
-//
-
 MAEVR.GUI = {
   elapsedDate: null,
   elapsedTimeDiv: null,
